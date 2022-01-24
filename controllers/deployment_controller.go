52dd880b94441d940fc761828ae19d9baaa89a61
danishprakash <grafitykoncept@gmail.com>
2022-01-24 16:13:38 +0530

    controller: handle max count leak, add funcdocs

--- controllers/deployment_controller.go
+++ controllers/deployment_controller.go
@@ -36,15 +36,23 @@ type DeploymentReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-const STEP_SIZE = 3
+// STEP_SIZE denotes the step
+// size for every subsequent scale
+// operation on a deployment
+const STEP_SIZE = 2
 
+// Config temporarily stores
+// goalReplicas for the specified
+// runtimes.
 type Config struct {
 	Replicas int
 }
 
+// Ideally, we'd delegate all this
+// configuration to the Custom Resource
 var config = map[string]Config{
-	"execute-d": Config{
-		Replicas: 50,
+	"nodeservice": Config{
+		Replicas: 10,
 	},
 }
 
@@ -66,118 +74,176 @@ func (r *DeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 
 	// we have current replica from the deployment
 	// final value would be the goal replicas to reach
+
+	// fetch the deployment manifest for
+	// which the controller was invoked
 	deployment := &appsv1.Deployment{}
 	err := r.Get(ctx, req.NamespacedName, deployment)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			// Object not found, return.  Created objects are automatically garbage collected.
-			// For additional cleanup logic use finalizers.
 			return reconcile.Result{}, nil
 		}
 		// Error reading the object - requeue the request.
+		if errors.IsResourceExpired(err) {
+			log.Info("conflict, re-queuing again")
+		}
 		return reconcile.Result{}, err
 	}
 
-	// TODO: for testing, only operate upon this
-	if deployment.Name != "execute-d" {
+	// for testing, we're skipping other deployments
+	if deployment.Name != "nodeservice" {
 		return ctrl.Result{}, nil
 	}
+
+	goalReplicas := config[deployment.Name].Replicas
 	currentReplicas := deployment.Status.Replicas
 	updatedReplicas := deployment.Status.UpdatedReplicas
 	availableReplicas := deployment.Status.AvailableReplicas
 
-	// Previous scale up event still pending
-	// NOTE: this is going to cause a infinite loop
-	// as it is right now, since the HPA will update the
-	// deployment and the deployment will trigger this controller
-
-	// IMP: we can check the available vs desired count in the deployment
-	// if that is not equal, we can return and let it come up, once the
-	// last pod has come up, we'll run the entire reconciliation logic
-	// again
-	// update deployment conditionally
+	// Checking two important conditions to skip the
+	// reconciliation process:
+	//   1. If the deployment has been updated previously
+	//      by the controller and is still waiting on pods
+	// 		to come up (updatedReplicas != availableReplicas)
+	//		If we were to skip this, our controller might
+	//		end up in an infinite loop trying to update
+	// 		the deployment and the subsequent updates would
+	//		trigger another reconciliation loop.
+	//   2. If the deployment has reached the final goal
+	//		replica after repeated reconciliations
+	//		i.e. availableReplicas == goalReplicas
+	//
+	// We're checking in two separate conditionals just
+	// to have the controller log the appropriate action
 	if updatedReplicas != availableReplicas {
-		// TODO: add logs here
+		log.Info("Deployment is waiting for pods to come up, skipping updates.", "deployment", deployment.Name)
+		return ctrl.Result{}, nil
+	} else if availableReplicas == int32(goalReplicas) {
+		log.Info("Deployment up to date, skipping further updates.", "deployment", deployment.Name)
 		return ctrl.Result{}, nil
 	}
 
-	namespacesName := types.NamespacedName{
-		Namespace: "default",
-		Name:      deployment.Name,
-	}
-
-	// get hpa associated with this deployment (same name)
-	// get current min max replicas from hpa
-	hpa := &autoscalingv1.HorizontalPodAutoscaler{}
-	err = r.Get(ctx, namespacesName, hpa)
+	// Fetch the HPA manifest corresponding to this deployment
+	// Here, we're assuming they have the same name so we're not
+	// referencing the deployment resource directly.
+	hpa, err := r.getHPA(ctx, deployment.Name)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			// Object not found, return.  Created objects are automatically garbage collected.
-			// For additional cleanup logic use finalizers.
+			// Object not found, return.
 			return reconcile.Result{}, nil
 		}
+
 		// Error reading the object - requeue the request.
 		return reconcile.Result{}, err
 	}
+	currMinReplicas := hpa.Spec.MinReplicas
+	currMaxReplicas := hpa.Spec.MaxReplicas
 
-	minReplicas := hpa.Spec.MinReplicas
-	maxReplicas := hpa.Spec.MaxReplicas
+	log.Info("Fetched HPA, calculating desiredReplicas", "HPA", hpa.Name)
 
-	// calculate desired replica count (single value for both min/max)
-	// dampen the value according to the step size as part of config
-	desiredReplicas := int(*minReplicas) * STEP_SIZE
+	// Calculate the desiredReplicas for this deployment
+	// based on the STEP_SIZE configuration option ideally
+	// part of a custom resource. Since for our use-case,
+	// we tend to set a single value for both min/max,
+	// hence the single value for computation.
+	//
+	// Also worth exploring is dampening of the desiredReplicas
+	// count if the delta is beyond a certain threshold (TODO)
+	desiredReplicas := int(*currMinReplicas) * STEP_SIZE
 
-	// if desired_value < current_max; desired_value = current_max
+	// If the desiredReplicas is lower than the
+	// currentReplicas, make sure we don't reduce
+	// the number of running replicas by lowering
+	// the new max replica count. This could
+	// end up having adverse impact on the application.
+	//
+	// In this case, it's okay to increase the min
+	// count to the current replica size.
 	if desiredReplicas < int(currentReplicas) {
 		desiredReplicas = int(currentReplicas)
 	}
 
-	newMinReplicas := desiredReplicas
-	newMaxReplicas := currentReplicas
-	if newMaxReplicas < maxReplicas {
-		newMaxReplicas = maxReplicas
-	} else {
-		newMaxReplicas = int32(desiredReplicas)
+	// If the desiredReplicas are lower than
+	// the current max replicas, there should
+	// not be a problem reaching this value.
+	// This should ideally happen for when the scaling
+	// has just started and we're running on our
+	// normal HPA numbers.
+	//
+	// For e.g. let's say a workload has min/max 10/100
+	// normally, and the controller comes up with a
+	// value of 50 as the first step, then we can
+	// match the max value here since we're already
+	// reaching this max (100) as part of our usual
+	// HPA operations.
+	if desiredReplicas < int(currMaxReplicas) {
+		desiredReplicas = int(currMaxReplicas)
 	}
 
-	if newMinReplicas > int(newMaxReplicas) {
-		newMinReplicas = int(newMaxReplicas)
+	// This is to ensure capping of the controller
+	// operations essentially by checking if
+	// the desiredReplicas comes out to be higher
+	// than our goalReplicas. Just cap it to goalReplicas.
+	if desiredReplicas > config[deployment.Name].Replicas {
+		desiredReplicas = config[deployment.Name].Replicas
 	}
 
-	// TODO: IMP: upper limit isn't being respected right now
-	// if final_value == desired_value: return
-	// we've reached our goal values, don't do anything
-	if config[deployment.Name].Replicas == desiredReplicas {
-		return ctrl.Result{}, nil
-	}
-
-	hpa.Spec.MaxReplicas = newMaxReplicas
-	tmp := int32(newMinReplicas)
-	hpa.Spec.MinReplicas = &tmp
-
-	// else: update HPA resource with the new min max values
-	// TODO: handle reconciliation errrors here
-	err = r.Update(ctx, hpa)
-	if err != nil && !errors.IsAlreadyExists(err) {
+	// Fetch HPA manifest again before updating it
+	// so that we get the most up to date copy of it.
+	// Changes that the controller makes to the
+	// deployment controller would also trigger
+	// HPA resource updates, so in order to avoid
+	// conflicts during updates with respect to
+	// validation (optimistic locking) from the API
+	// server, be prudent and fetch the manifest again.
+	hpa, err = r.getHPA(ctx, deployment.Name)
+	if err != nil {
 		if errors.IsNotFound(err) {
-			// Object not found, return.  Created objects are automatically garbage collected.
-			// For additional cleanup logic use finalizers.
-			log.Info("HPA not found, requeuing request")
+			// Object not found, return.
 			return reconcile.Result{}, nil
 		}
-
-		log.Info("HPA resourceVersion didn't match, requeuing request")
 		// Error reading the object - requeue the request.
 		return reconcile.Result{}, err
 	}
 
-	log.Info("values", "newMinReplicas", newMinReplicas, "newMaxReplicas", newMaxReplicas)
+	// Update the HPA resource with the
+	// desired min/max values (desiredReplicas)
+	t := int32(desiredReplicas)
+	hpa.Spec.MinReplicas = &t
+	hpa.Spec.MaxReplicas = int32(desiredReplicas)
 
-	// at any point, we're not going to make changes to the
-	// the deployment resource, that will be taken care of
-	// by the HPA resource
+	// Update the HPA for the current deployment
+	log.Info("Updating HPA", "HPA", deployment.Name, "min/max", desiredReplicas)
+	err = r.Update(ctx, hpa)
+	if err != nil && !errors.IsAlreadyExists(err) {
+		if errors.IsNotFound(err) {
+			log.Info("HPA not found, requeuing request")
+			return reconcile.Result{}, nil
+		}
+
+		if errors.IsConflict(err) {
+			log.Info("HPA resourceVersion didn't match, requeuing request")
+			return reconcile.Result{}, nil
+		}
+
+		return reconcile.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *DeploymentReconciler) getHPA(ctx context.Context, name string) (*autoscalingv1.HorizontalPodAutoscaler, error) {
+	namespacesName := types.NamespacedName{
+		Namespace: "default",
+		Name:      name,
+	}
+
+	hpa := &autoscalingv1.HorizontalPodAutoscaler{}
+	err := r.Get(ctx, namespacesName, hpa)
+	if err != nil {
+		return nil, err
+	}
+	return hpa, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
